bf242d0a2a1c13b771c091429defa0115b2fb255
Corrado <corrado.torino@gmail.com>
2020-09-05 22:45:17 +0200

    restructure folder and add icon

--- AstronomyPictureOfTheDay.UI/MainWindow.cs
+++ AstronomyPictureOfTheDay.UI/MainWindow.cs
@@ -2,11 +2,13 @@
 // // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
-
 
 namespace AstronomyPictureOfTheDay
 {
@@ -20,6 +22,8 @@ namespace AstronomyPictureOfTheDay
         private readonly Downloader downloader = new Downloader();
 
         private CancellationTokenSource cancellationTokenSource;
+
+        private List<DateTime> unsupportedDays;
             
         public MainWindow()
         {
@@ -30,15 +34,16 @@ namespace AstronomyPictureOfTheDay
         {
             DebugUtils.WriteLine("Entering DatePicker_OnSelectedDateChanged");
 
+            var selectedDate = this.DatePicker.SelectedDate.GetValueOrDefault(DateTime.Now);
+
             this.cancellationTokenSource = new CancellationTokenSource();
 
             this.DisplayTransitionScreen();
-
+            
             try
             {
                 this.astronomyPictureOfTheDay =
-                    await this.downloader.DownloadDefinitionForAstronomyPictureOfTheDay(this.DatePicker.SelectedDate,
-                        this.cancellationTokenSource.Token);
+                    await this.downloader.DownloadDefinitionForAstronomyPictureOfTheDay(selectedDate, this.cancellationTokenSource.Token);
                 DebugUtils.WriteLine("Continue after DownloadDefinitionForAstronomyPictureOfTheDay");
 
                 await this.downloader.DownloadImage(this.astronomyPictureOfTheDay, this.DownloadingProgressBar,
@@ -48,7 +53,7 @@ namespace AstronomyPictureOfTheDay
                 this.UpdateUi(
                     this.astronomyPictureOfTheDay.title,
                     this.astronomyPictureOfTheDay.explanation,
-                    $".\\Samples\\APOD_{DatePicker.SelectedDate:yyyy-MM-dd}.jpg");
+                    $".\\Samples\\APOD_{selectedDate:yyyy-MM-dd}.jpg");
             }
             catch (OperationCanceledException)
             {
@@ -57,17 +62,27 @@ namespace AstronomyPictureOfTheDay
                     $"The operation was cancelled as requested.",
                     "StopWaiting.jpg");
             }
+            catch (NotSupportedException)
+            {
+                this.unsupportedDays.Add(selectedDate);
+                this.UpdateUi(
+                    "Not supported media format.",
+                    $"This day contain a not supported media format. This version load only image.",
+                    "NotSupported.jpg");
+            }
             catch (Exception ex)
             {
                 var innerMessage = ex.InnerException != null
                     ? $".{Environment.NewLine}{ex.InnerException.Message}"
                     : "";
-                
+
                 this.UpdateUi(
                     "Ops.. Something went wrong..",
                     $"{ex.Message}{innerMessage}",
                     "ErrorOccured.jpg");
             }
+
+            this.setBlackoutDays();
         }
         
         private void UpdateUi(string title, string explanation, string imagePath)
@@ -111,6 +126,69 @@ namespace AstronomyPictureOfTheDay
         private void CancelButton_OnClick(object sender, RoutedEventArgs e)
         {
             this.cancellationTokenSource.Cancel();
+        }
+
+        private async void DatePicker_OnCalendarOpened(object sender, RoutedEventArgs e)
+        {
+            if (this.unsupportedDays != null)
+            {
+                return;
+            }
+            
+            this.DatePicker.DisplayDateEnd = DateTime.Now;
+            this.unsupportedDays = new List<DateTime>();
+
+            await foreach (var date in GetUnsupportedDays())
+            {
+                if (this.DatePicker.SelectedDate.GetValueOrDefault(DateTime.MinValue) != date)
+                {
+                    this.DatePicker.BlackoutDates.Add(new CalendarDateRange(date, date));
+                }
+                else
+                {
+                    this.unsupportedDays.Add(date);
+                }
+            }
+        }
+
+        private void setBlackoutDays()
+        {
+            foreach (var day in this.unsupportedDays)
+            {
+                if (this.DatePicker.SelectedDate.GetValueOrDefault(DateTime.MinValue) != day)
+                {
+                    this.DatePicker.BlackoutDates.Add(new CalendarDateRange(day, day));
+                }
+            }
+        }
+
+        private async IAsyncEnumerable<DateTime> GetUnsupportedDays()
+        {
+            var apodFiles = Directory.GetFiles(".\\Samples", "APOD_*.json");
+
+            foreach (var file in apodFiles)
+            {
+                DateTime? day = null;
+                try
+                {
+                    var apodResponseAsString = await File.ReadAllTextAsync(file);
+                    DebugUtils.WriteLine("Continue after File.ReadAllTextAsync");
+
+                    var apodResponse = JsonSerializer.Deserialize<AstronomyPictureOfTheDayResponse>(apodResponseAsString);
+                    if (apodResponse.media_type != "image")
+                    {
+                        day = apodResponse.date;
+                    }
+                }
+                catch (JsonException)
+                {
+                }
+
+                if (day.HasValue)
+                {
+                    yield return day.Value;
+                }
+            }
         }
     }
 }
